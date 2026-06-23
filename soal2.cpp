7538837a1920cff7aa410a3aab6d7c3c05ab6016
elinanadika <alyrein847@gmail.com>
2026-06-23 21:22:02 +0800

    .

--- soal2.cpp
+++ soal2.cpp
@@ -1,3 +1,48 @@
 #include <iostream>
 #include <string>
+#include <bits/stdc++.h>
 using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<string> label(n);
+    for (int i = 0; i < n; i++) {
+        cin >> label[i];
+    }
+
+    vector<vector<int>> mat(n, vector<int>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> mat[i][j];
+        }
+    }
+
+    cout << "Adjacency List:" << "\n";
+
+    for (int i = 0; i < n; i++) {
+        cout << label[i] << " -> ";
+
+        bool adaEdge = false;   
+        string hasil = "";      
+
+        for (int j = 0; j < n; j++) {
+            if (mat[i][j] > 0) {
+                if (adaEdge) {
+                    hasil += ", ";
+                }
+                hasil += "(" + label[j] + "," + to_string(mat[i][j]) + ")";
+                adaEdge = true;
+            }
+        }
+
+        if (adaEdge) {
+            cout << hasil << "\n";
+        } else {
+            cout << "-" << "\n";
+        }
+    }
+
+    return 0;
+}
